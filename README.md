f16e9e48d343c076835377da691181748a0ea264
omarfahmy555 <omaffhj@gmail.com>
2023-03-11 19:55:01 +0200

    My First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# ChatGPT AI Chatbot App
+
+
+<p>
+A Chatbot chat app built using the Flutter framework and OpenAI's GPT-3 language model.
+</p>
+
+### Show some :heart: and star the repo.
+
+## [Video tutorial available here](https://youtu.be/94JmNb1IhX0)
+
+##[Github link](https://github.com/iampawan/ChatGPT-Flutter-AIChatBot)
+
+##  [Link for api](https://beta.openai.com/account/api-keys)
+
+## Features
+
+- Natural language understanding
+- Human-like conversation
+- Customizable to fit your use-case
+- Lightweight, easy to integrate with other apps
+- Open-source
+
+## Prerequisites
+
+- Flutter SDK
+- API key for OpenAI GPT-3 [Available here](https://beta.openai.com/account/api-keys)
+
+## Getting Started
+
+1. Clone the repository
+2. Run `flutter pub get` to install dependencies
+3. Replace the placeholder API key with your own in the `chat_screen.dart` file
+4. Run the app on an emulator or physical device
+
+The chatbot can be further customized to fit your use-case by modifying the code in the `main.dart` file and other files. The source code for the chatbot is available on GitHub.
+
+### Development by
+
+- OpenAI - https://openai.com
+- Flutter - https://flutter.dev/
+
+Please open an issues on the repository if you find any bugs or if you would like to contribute.
+
---
+++ lib/chat_screen.dart
@@ -0,0 +1,131 @@
+import 'dart:async';
+
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'chat_message.dart';
+import 'threedots.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  ChatGPT? chatGPT;
+
+  StreamSubscription? _subscription;
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    super.initState();
+    chatGPT = ChatGPT.instance;
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  // Link for api - https://beta.openai.com/account/api-keys
+
+  void _sendMessage() {
+    if (_controller.text.isEmpty) return;
+    ChatMessage message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+      _isTyping = true;
+    });
+
+    _controller.clear();
+
+    final request = CompleteReq(prompt: message.text, model: kTranslateModelV3, max_tokens: 200);
+
+    _subscription = chatGPT!
+        .builder("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")
+        .onCompleteStream(request: request)
+        .listen((response) {
+      //log message
+      Vx.log(response!.choices[0].text);
+
+      ChatMessage botMessage = ChatMessage(text: response.choices[0].text, sender: "Sweet ChatBot");
+
+      setState(() {
+        _messages.insert(0, botMessage);
+      });
+    });
+
+  }
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            onSubmitted: (value) => _sendMessage(),
+            decoration: const InputDecoration.collapsed(
+                hintText: "Enter message here"),
+          ),
+        ),
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                _sendMessage();
+              },
+            ),
+          ],
+        ),
+      ],
+    ).px16();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("ChatGPT APP",),
+          centerTitle: true,
+          backgroundColor: Colors.brown,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold,),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Flexible(
+                  child: ListView.builder(
+                reverse: true,
+                padding: Vx.m8,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              )),
+              if (_isTyping) const ThreeDots(),
+              const Divider(
+                height: 1.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: context.cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+        )
+        );
+  }
+}
